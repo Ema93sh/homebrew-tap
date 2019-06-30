5f966a478dc6128ce4b13d5c08368dcdf257c556
ema93sh <m.magesh.66@gmail.com>
2019-06-30 22:39:59 +0000

    Update gclean formula

--- gclean.rb
+++ gclean.rb
@@ -1,8 +1,8 @@
 class Gclean < Formula
   desc "CLI for cleaning you os"
   homepage "https://gclean.mage.sh"
-  url "https://storage.googleapis.com/gclean/0.0.10/gclean.tar.gz"
-  sha256 "69a22dc727af5d4dad845d167eed633c680dce83721fd14be15ab6a6d803a263"
+  url "https://storage.googleapis.com/gclean/0.0.11/gclean.tar.gz"
+  sha256 "b174cc7a64706dbea3a855841f380e89fb60afbb5cde1c5284b92c57ad2c7b80"
   depends_on "go" => :build
 
   def install
@@ -11,11 +11,11 @@ class Gclean < Formula
     bin_path = buildpath/"src/github.com/ema93sh/gclean"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "go", "build", "-o", bin/"gclean", "-ldflags=-X main.Version=0.0.10", "."
+      system "go", "build", "-o", bin/"gclean", "-ldflags=-X main.Version=0.0.11", "."
     end
   end
 
   test do
-    assert_match "gclean version 0.0.10", shell_output("#{bin}/gclean -v 2>&1", 0)
+    assert_match "gclean version 0.0.11", shell_output("#{bin}/gclean -v 2>&1", 0)
   end
 end
